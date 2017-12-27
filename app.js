fdfd13f398dc93da6765d6b03794949745154144
Eric Putnam <putnam.eric@gmail.com>
2017-12-27 17:17:22 -0500

    clean up app.js and update gitignore to ignore new yarn.lock file

--- app.js
+++ app.js
@@ -3,29 +3,26 @@ var path = require('path');
 var app = express();
 var router = express.Router();
 
-app.set('view engine', 'pug', function() {
-    console.log("View engine is pug");
-});
+app.set('view engine', 'pug');
 
-app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "public")));
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 router.use(function (req,res,next) {
-  console.log("/" + req.method);
+  console.log('/' + req.method);
   next();
 });
 
-router.get("/",function(req,res){
-  res.render("/views/index.pug");
+router.get('/',function(req,res){
+  res.render('/views/index.pug');
 });
 
-app.use("/",router);
+app.use('/',router);
 
-// app.use("*",function(req,res){
-//   res.sendFile(path + "404.html");
+// app.use('*',function(req,res){
+//   res.sendFile(path + '404.html');
 // });
 
 app.listen(process.env.PORT || 3000,function(){
-  console.log("Live at Port 3000");
+  console.log('Live at Port 3000');
 });
-
